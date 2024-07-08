ee44b5d0fb15a33e063db42622184946f8e44bac
Rango-lzs <1191472748@qq.com>
2024-07-08 09:09:07 +0800

    add gtest sub module

--- lib/cmake/GTest/GTestTargets.cmake
+++ lib/cmake/GTest/GTestTargets.cmake
@@ -54,7 +54,7 @@ endif()
 add_library(GTest::gtest STATIC IMPORTED)
 
 set_target_properties(GTest::gtest PROPERTIES
-  INTERFACE_COMPILE_FEATURES "cxx_std_11"
+  INTERFACE_COMPILE_FEATURES "cxx_std_14"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
@@ -63,7 +63,7 @@ set_target_properties(GTest::gtest PROPERTIES
 add_library(GTest::gtest_main STATIC IMPORTED)
 
 set_target_properties(GTest::gtest_main PROPERTIES
-  INTERFACE_COMPILE_FEATURES "cxx_std_11"
+  INTERFACE_COMPILE_FEATURES "cxx_std_14"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_LINK_LIBRARIES "GTest::gtest"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
@@ -73,7 +73,7 @@ set_target_properties(GTest::gtest_main PROPERTIES
 add_library(GTest::gmock STATIC IMPORTED)
 
 set_target_properties(GTest::gmock PROPERTIES
-  INTERFACE_COMPILE_FEATURES "cxx_std_11"
+  INTERFACE_COMPILE_FEATURES "cxx_std_14"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
@@ -82,7 +82,7 @@ set_target_properties(GTest::gmock PROPERTIES
 add_library(GTest::gmock_main STATIC IMPORTED)
 
 set_target_properties(GTest::gmock_main PROPERTIES
-  INTERFACE_COMPILE_FEATURES "cxx_std_11"
+  INTERFACE_COMPILE_FEATURES "cxx_std_14"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
